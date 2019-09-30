15f48ce15ff792f8337dd84e2d6e43d190ade4a0
chengganghy <chenggang@ruyucapital.com>
2019-09-30 11:21:54 +0800

    环境搭建

--- vue.config.js
+++ vue.config.js
@@ -16,14 +16,5 @@ module.exports = {
                 }
             }
         },
-    },
-    configureWebpack: {
-        externals: {
-            'vue': 'Vue',
-            'vue-router': 'VueRouter',
-            'vuex': 'Vuex',
-            'axios': 'axios',
-            'element-ui': 'ELEMENT'
-        }
     }
 }
